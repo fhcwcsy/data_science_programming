34a1dabd8a1afb2957e08aa967f5c6ebf830560c
fhcwcsy <fhcwcsy@gmail.com>
2019-07-08 22:59:50 +0800

    R syntax practice

--- week_1/test.R
+++ week_1/test.R
@@ -1,6 +1,5 @@
-library (gapminder)
-library (dplyr)
-
-gapminder %>%
-	filter (year == 1957) %>%
-	arrange (desc(pop))
+# data <- read.table("test_data.csv", header = TRUE, sep = ",")
+# data
+x <- c(1, 5, 12, 18, 19, 21, 25, 31)
+cut(x, c(0, 10, 20, 30, 40), c(5, 15, 25, 35))
+class ( write.table(data, file = "test_output.csv", sep = " ") )
